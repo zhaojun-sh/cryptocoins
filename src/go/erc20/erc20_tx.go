05066545bdef1d9db55fd08e79ee3acb35bf594d
gaozhengxin <zhengxingao@live.cn>
2019-01-15 19:44:39 +0800

    erc20: modify params, add methods GetTransactionInfo, GetAddressBalance

--- src/go/erc20/erc20_tx.go
+++ src/go/erc20/erc20_tx.go
@@ -3,23 +3,37 @@ package erc20
 import  (
 	"context"
 	"crypto/ecdsa"
+	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"math/big"
+	"strings"
 
-	ethcrypto "github.com/fusion/go-fusion/crypto"
+	"github.com/fusion/go-fusion/params"
+
 
 	"github.com/fusion/go-fusion"
+	"github.com/fusion/go-fusion/accounts/abi"
+	"github.com/fusion/go-fusion/accounts/abi/bind"
 	"github.com/fusion/go-fusion/common"
 	"github.com/fusion/go-fusion/core/types"
+	ethcrypto "github.com/fusion/go-fusion/crypto"
 	"github.com/fusion/go-fusion/crypto/sha3"
 	"github.com/fusion/go-fusion/ethclient"
+
+	"github.com/cryptocoins/src/go/config"
+	rpcutils "github.com/cryptocoins/src/go/rpcutils"
+
+	"github.com/cryptocoins/src/go/erc20/token"
 )
 
 var (
-	url = "http://54.183.185.30:8018"
+	url = config.ETH_GATEWAY
 	err error
+	chainConfig = params.RinkebyChainConfig
 )
 
 var tokens map[string]string = map[string]string{
@@ -33,7 +47,7 @@ var tokens map[string]string = map[string]string{
 type ERC20TransactionHandler struct {
 }
 
-func (h ERC20TransactionHandler) PublicKeyToAddress (pubKeyHex string) (address string, err error) {
+func (h ERC20TransactionHandler) PublicKeyToAddress (pubKeyHex string) (address string, msg string, err error) {
 	data := hexEncPubkey(pubKeyHex[2:])
 
 	pub, err := decodePubkey(data)
@@ -45,13 +59,39 @@ func (h ERC20TransactionHandler) PublicKeyToAddress (pubKeyHex string) (address 
 //args[0]: gasPrice	*big.Int
 //args[1]: gasLimit	uint64
 //args[2]: tokenType	string
-func (h ERC20TransactionHandler) BuildUnsignedTransaction (fromAddress, toAddress string, amount *big.Int, args ...interface{}) (transaction interface{}, digests []string, err error) {
+func (h ERC20TransactionHandler) BuildUnsignedTransaction (fromAddress, fromPublicKey, toAddress string, amount *big.Int, args ...interface{}) (transaction interface{}, digests []string, err error) {
 	client, err := ethclient.Dial(url)
 	if err != nil {
 		return
 	}
 	transaction, hash, err := erc20_newUnsignedTransaction(client, fromAddress, toAddress, amount, args[0].(*big.Int), args[1].(uint64), args[2].(string))
-	digests = append(digests, hash.Hex())
+	hashStr := hash.Hex()
+	if hashStr[:2] == "0x" {
+		hashStr = hashStr[2:]
+	}
+	digests = append(digests, hashStr)
+	return
+}
+
+/*
+func SignTransaction(hash string, address string) (rsv string, err error) {
+	return
+}
+*/
+
+func (h ERC20TransactionHandler) SignTransaction(hash []string, privateKey interface{}) (rsv []string, err error) {
+	hashBytes, err := hex.DecodeString(hash[0])
+	if err != nil {
+		return
+	}
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey.(*ecdsa.PrivateKey), hashBytes)
+	if err != nil {
+		return
+	}
+	fmt.Printf("r: %v\ns: %v\n\n", r, s)
+	rx := fmt.Sprintf("%X", r)
+	sx := fmt.Sprintf("%X", s)
+	rsv = append(rsv, rx + sx + "00")
 	return
 }
 
@@ -69,6 +109,94 @@ func (h ERC20TransactionHandler) SubmitTransaction(signedTransaction interface{}
 		return
 	}
 	return erc20_sendTx(client, signedTransaction.(*types.Transaction))
+}
+
+func (h ERC20TransactionHandler) GetTransactionInfo(txhash string) (fromAddress, toAddress string, transferAmount *big.Int, _ []interface{}, err error) {
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		return
+	}
+	hash := common.HexToHash(txhash)
+	tx, isPending, err1 := client.TransactionByHash(context.Background(), hash)
+	if err1 == nil && isPending == false && tx != nil {
+		msg, err2 := tx.AsMessage(types.MakeSigner(chainConfig, big.NewInt(3504188)))
+		err = err2
+		fromAddress = msg.From().Hex()
+		data := msg.Data()
+
+		toAddress, transferAmount, err = DecodeTransferData(data)
+
+	} else if err1 != nil {
+		err = err1
+	} else if isPending {
+		err = fmt.Errorf("Transaction is pending")
+	} else {
+		err = fmt.Errorf("Unknown error")
+	}
+	return
+}
+
+// args[0] coinType string
+func (h ERC20TransactionHandler) GetAddressBalance(address string, args ...interface{}) (balance *big.Int, err error) {
+	if args[0] == nil {
+		err = fmt.Errorf("Unspecified coin type")
+		return
+	}
+
+	tokenAddr := tokens[args[0].(string)]
+	if tokenAddr == "" {
+		err = fmt.Errorf("Token not supported")
+		return
+	}
+
+	myABIJson := `[{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}]`
+	myABI, err := abi.JSON(strings.NewReader(myABIJson))
+	if err != nil {
+		return
+	}
+
+	data, err := myABI.Pack("balanceOf", common.HexToAddress(address))
+	if err != nil {
+		return
+	}
+	dataHex := "0x" + hex.EncodeToString(data)
+	fmt.Printf("data is %v\n\n", dataHex)
+
+	reqJson := `{"jsonrpc": "2.0","method": "eth_call","params": [{"to": "` + tokenAddr + `","data": "` + dataHex + `"},"latest"],"id": 1}`
+	fmt.Printf("reqJson: %v\n\n", reqJson)
+
+	ret := rpcutils.DoPostRequest2(url, reqJson)
+	fmt.Printf("ret: %v\n\n", ret)
+
+	var retStruct map[string]interface{}
+	json.Unmarshal([]byte(ret), &retStruct)
+	if retStruct["result"] == nil {
+		if retStruct["error"] != nil {
+			err = fmt.Errorf(retStruct["error"].(map[string]interface{})["message"].(string))
+			return
+		}
+		err = fmt.Errorf(ret)
+		return
+	}
+	balanceStr := retStruct["result"].(string)[2:]
+	balanceHex, _ := new(big.Int).SetString(balanceStr, 16)
+	balance, _ = new(big.Int).SetString(fmt.Sprintf("%d",balanceHex), 10)
+
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tokenAddress := common.HexToAddress(tokenAddr)
+	instance, err := token.NewToken(tokenAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	balance1, _ := instance.BalanceOf(&bind.CallOpts{}, common.HexToAddress(address))
+	fmt.Printf("balance1: %v\n\n", balance1)
+
+	return
 }
 
 func hexEncPubkey(h string) (ret [64]byte) {
@@ -95,38 +223,22 @@ func decodePubkey(e [64]byte) (*ecdsa.PublicKey, error) {
 	return p, nil
 }
 
-/*func main () {
-	//获取dcrm地址
-	dcrmAddress := "0xA8dC61209400C9A23bf1fe625c2919c3626Bc157"
-	//toAddressHex := "0x7b5Ec4975b5fB2AA06CB60D0187563481bcb6140"
-	toAddressHex := ""
-	amount, _ := new(big.Int).SetString("1",10)
-	gasLimit := uint64(0)
-	tx, txhash, err := Erc20_newUnsignedTransaction(client, dcrmAddress, toAddressHex, amount, nil, gasLimit, "BNT")
-	if err != nil {
-		fmt.Printf("%v\n",err)
+func DecodeTransferData(data []byte) (toAddress string, transferAmount *big.Int, err error) {
+	eventData := data[:4]
+	if string(eventData) == string([]byte{0xa9, 0x05, 0x9c, 0xbb}) {
+		addressData := data[4:36]
+		amountData := data[36:]
+		num, _ := new(big.Int).SetString(hex.EncodeToString(addressData), 16)
+		toAddress = "0x" + fmt.Sprintf("%x", num)
+		amountHex, _ := new(big.Int).SetString(hex.EncodeToString(amountData), 16)
+		transferAmount, _ = new(big.Int).SetString(fmt.Sprintf("%d", amountHex), 10)
+	} else {
+		err = fmt.Errorf("Invalid transfer data")
 		return
 	}
-	fmt.Println("tx is ",tx)
-	fmt.Println("txhash is ", txhash.String())
-
-	//dcrm 签名
-	rsv := "26C729F4B7C1D0407BB0B8D1052771B20ED3DC96739EDC2694684EF3FCA935735B38E837D6BAE347BFE2B29772211C3FE66164B72B1FA23F612A4B9148D013C500"
-	signedtx, err := MakeSignedTransaction(client, tx, rsv)
-	if err != nil {
-		fmt.Printf("%v\n",err)
-		return
-	}
-
-	res, err := Erc20_sendTx(client, signedtx)
-	if err != nil {
-		fmt.Printf("====== error: %v ======\n",err)
-		return
-	}
-	fmt.Printf("sent transaction : %s\n", res)
-
+	return
 }
-*/
+
 func erc20_newUnsignedTransaction (client *ethclient.Client, dcrmAddress string, toAddressHex string, amount *big.Int, gasPrice *big.Int, gasLimit uint64, tokenType string) (*types.Transaction, *common.Hash, error) {
 
 	chainID, err := client.NetworkID(context.Background())
@@ -214,5 +326,5 @@ func erc20_sendTx (client *ethclient.Client, signedTx *types.Transaction) (strin
 	if err != nil {
 		return "", err
 	}
-	return signedTx.Hash().Hex(), nil
+	return "success/" + signedTx.Hash().Hex(), nil
 }
