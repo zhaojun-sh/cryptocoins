f64d69b84cf47589dd9b30c84b2a12e5c9c777fb
gaozhengxin <zhengxingao@live.cn>
2019-01-15 19:38:49 +0800

    modify api

--- src/go/api.go
+++ src/go/api.go
@@ -3,18 +3,25 @@ package main
 import (
 	"math/big"
 
-	"cryptocoins/src/go/erc20"
+	"github.com/cryptocoins/src/go/erc20"
+	"github.com/cryptocoins/src/go/xrp"
 )
 
 type TransactionHandler interface {
 
 	// 公钥to地址
-	PublicKeyToAddress(pubKeyHex string) (address string, err error)
-
-	//GetPublicKey(pubKeyHex string) (publicKey interface{}, err error)
+	// eos的address是随机生成的账户名, msg是eos格式的公钥: EOS6JUDHVf4mbrbMNXxhMVJUj5Tz14d1jYpdjC8ZvRgFb4jhrBKEe
+	PublicKeyToAddress(pubKeyHex string) (address string, msg string, err error)
 
 	// 构造未签名交易
-	BuildUnsignedTransaction(fromAddress, toAddress string, amount *big.Int, args ...interface{}) (transaction interface{}, digests []string, err error)
+	// btc, ripple需要fromPublicKey
+	// eth, erc20需要fromAddress
+	// eos需要账户名(fromAddress)和fromPublicKey
+	BuildUnsignedTransaction(fromAddress, fromPublicKey, toAddress string, amount *big.Int, args ...interface{}) (transaction interface{}, digests []string, err error)
+
+	// 签名函数 txhash 输出 rsv
+	//SignTransaction(hash []string, address string) (rsv []string, err error)
+	SignTransaction(hash []string, privateKey interface{}) (rsv []string, err error)
 
 	// 构造签名交易
 	MakeSignedTransaction(rsv []string, transaction interface{}) (signedTransaction interface{}, err error)
@@ -22,26 +29,29 @@ type TransactionHandler interface {
 	// 提交交易
 	SubmitTransaction(signedTransaction interface{}) (ret string, err error)
 
-	//SetCrypto(cryptoType string)
+	// txhash 查出 fromaddress， toaddress， 交易额
+	GetTransactionInfo(txhash string) (fromAddress, toAddress string, transferAmount *big.Int, _ []interface{}, err error)
+
+	// 账户查账户余额
+	GetAddressBalance(address string, args ...interface{}) (balance *big.Int, err error)
+
+	//SetCrypto(cryptoType string)  // ecdsa, ed25519
 	//GetCrypto() string
-	//SetCurve(curve string)
+	//SetCurve(curve string)  // k1, r1
 	//GetCurve() string
-	//Canonical() bool
 }
 
 func NewTransactionHandler(coinType string) (txHandler TransactionHandler) {
 	switch coinType {
 	case "ERC20":
 		return &erc20.ERC20TransactionHandler{}
+	case "XRP":
+		return &xrp.XRPTransactionHandler{}
 /*
 	case "BTC":
 		return &BTCTransactionHandler{}
 	case "ETH":
 		return &ETHTransactionHandler{}
-	case "ERC20":
-		return &ERC20TransactionHandler{}
-	case "XRP":
-		return &XRPTransactionHandler{}
 	case "EOS":
 		return &EOSTransactionHandler{}
 */
