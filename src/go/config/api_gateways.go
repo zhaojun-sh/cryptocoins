6b31f05edcf6bfd1965eb185871d516fb51858e1
gaozhengxin <zhengxingao@live.cn>
2019-01-16 17:16:59 +0800

    add eos configs

--- src/go/config/api_gateways.go
+++ src/go/config/api_gateways.go
@@ -2,7 +2,9 @@
 package config
 
 const (
-	ETH_GATEWAY = "http://54.183.185.30:8018"
-	XRP_GATEWAY = "https://s.altnet.rippletest.net:51234"
+	ETH_GATEWAY = "http://54.183.185.30:8018"  // Rinkeby test net
+	XRP_GATEWAY = "https://s.altnet.rippletest.net:51234"  // Ripple test net
+	EOS_NODEOS = "https://api.kylin.alohaeos.com"  // cryptokylin test net
+	EOS_CHAIN_ID = "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"  // cryptokylin test net
 )
 
