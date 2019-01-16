7b7374e9be5ce0f8292aacd87d3fd1dfc567c59b
gaozhengxin <zhengxingao@live.cn>
2019-01-16 17:14:40 +0800

    add support for eos

---
+++ src/go/eos/config.go
@@ -0,0 +1,27 @@
+package eos
+import (
+	eos "github.com/eoscanada/eos-go"
+
+	"github.com/cryptocoins/src/go/config"
+)
+
+var (
+	nodeos = config.EOS_NODEOS
+
+	opts = &eos.TxOptions{
+		ChainID: hexToChecksum256(config.EOS_CHAIN_ID),
+		MaxNetUsageWords: uint32(500),
+		//DelaySecs: uint32(120),
+		//MaxCPUUsageMS:
+		Compress: eos.CompressionNone,
+	}
+)
+
+const EOS_ACCURACY = 10000
+
+const ALPHABET = "defghijklmnopqrstuvwxyz12345abcdefghijklmnopqrstuvwxyz12345abc"
+
+
+
+
+
