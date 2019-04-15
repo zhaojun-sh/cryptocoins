c08edfc1b00afc0f4b68524699dda38235cdf02b
gaozhengxin <zhengxingao@live.cn>
2019-04-15 14:50:21 +0800

    fix bug: eos address pattern incorrect

--- src/go/validAddress.go
+++ src/go/validAddress.go
@@ -8,7 +8,7 @@ var RegExpmap map[string]string = map[string]string {
 	"BTC":"^(1|3|m|n)[a-zA-Z\\d]{24,33}$",
 	"ETH":"^(0x)?[0-9a-fA-F]{40}$",
 	"XRP":"^r[1-9a-km-zA-HJ-NP-Z]{33}$",
-	"EOS":"^d[a-z\\d]{34}$",
+	"EOS":"^d[a-z\\d]{33}$",
 }
 
 type AddressValidator struct {
