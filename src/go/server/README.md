a619472df4444169c606c732aed2dad7c39ec09c
gaozhengxin <zhengxingao@live.cn>
2019-05-14 16:49:37 +0800

    add http server, build any address and query any transaction

---
+++ src/go/server/README.md
@@ -0,0 +1,3 @@
+```
+http://0.0.0.0:23333/gettransaction?txhash=1b872f4c434f1d41b92fcf51cf9ac0d34c4b4ea1c4119407ad9e091c9ba8c323&cointype=BTC
+```
---
+++ src/go/server/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"flag"
+	"github.com/gaozhengxin/cryptocoins/src/go/types"
+	api "github.com/gaozhengxin/cryptocoins/src/go"
+)
+
+func main () {
+	port := flag.String("port","23333","port")
+	flag.Parse()
+	path := "0.0.0.0:" + *port
+	http.HandleFunc("/gettransaction", GetTransaction)
+	http.HandleFunc("/pubkeytoaddress", PubkeyToAddress)
+	go http.ListenAndServe(path, nil)
+	fmt.Printf("service is running on %s\n", path)
+	select{}
+}
+
+type Resp struct {
+	Code string `json:"code"`
+	Msg string `json:"Msg,omitempty"`
+	Result *GetTxResult `json:"result,omitempty"`
+}
+
+type Resp2 struct {
+	Code string `json:"code"`
+	Msg string `json:"Msg,omitempty"`
+	Result PubkeyToAddrResult `json:"result,omitempty"`
+}
+
+var coinlist []string = []string{"BTC","ETH","ERC20BNB","ERC20HT","TRX","XRP"}
+
+type PubkeyToAddrResult map[string]string
+
+type GetTxResult struct {
+	FromAddress string `json:"FromAddress"`
+	TxOutputs []types.TxOutput `json:"TxOutputs,omitempty"`
+	Err string `json:"Error,omitempty"`
+}
+
+func PubkeyToAddress (writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	pubkey, ok := request.Form["pubkey"]
+	var result Resp2
+	if !ok {
+		result.Code = "401"
+		result.Msg = "require pubkey"
+	} else {
+		result.Result = make(map[string]string)
+		for _, cointype := range coinlist {
+			h := api.NewCryptocoinHandler(cointype)
+			address, err := h.PublicKeyToAddress(pubkey[0])
+			if err != nil {
+				result.Code = "500"
+				result.Msg = err.Error()
+				return
+			}
+			result.Result[cointype] = address
+		}
+		result.Code = "200"
+	}
+	if err := json.NewEncoder(writer).Encode(result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetTransaction (writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	txhash, ok1 := request.Form["txhash"]
+	cointype, ok2 := request.Form["cointype"]
+	var result Resp
+	if !ok1 {
+		result.Code = "401"
+		result.Msg = "require txhash"
+	} else if !ok2 {
+		result.Code = "401"
+		result.Msg = "require cointype"
+	} else {
+		result.Code = "200"
+		h := api.NewCryptocoinHandler(cointype[0])
+		fromAddress, txOutputs, _, err := h.GetTransactionInfo(txhash[0])
+		result.Result = &GetTxResult{
+			FromAddress: fromAddress,
+			TxOutputs: txOutputs,
+		}
+		if err != nil {
+			result.Result.Err = err.Error()
+		}
+	}
+	if err := json.NewEncoder(writer).Encode(result); err != nil {
+		log.Fatal(err)
+	}
+}
